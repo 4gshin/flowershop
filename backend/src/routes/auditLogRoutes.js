8ad21f2ac8d8bbeec8c84b57052bc1a0d7554a4e
Agshin Heybatli <heybatli2007@gmail.com>
2026-08-05 13:58:30 +0400

    Audit Fix 5

--- backend/src/routes/auditLogRoutes.js
+++ backend/src/routes/auditLogRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const orderRoutes = require('./routes/orderRoutes');
-const auditLogRoutes = require('./routes/auditLogRoutes'); // <-- 1. BUNU ƏLAVƏ ET
 const router = express.Router();
 const { getAuditLogs } = require('../controllers/auditLogController');
-const { protect, adminOnly } = require('../middleware/authMiddleware'); // Təhlükəsizlik middleware-ləri
+const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 // Yalnız autentifikasiyadan keçmiş adminlər baxa bilər
 router.get('/', protect, adminOnly, getAuditLogs);

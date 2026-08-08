5836137b28ee8660b35b14544063be54f2a91487
Agshin Heybatli <heybatli2007@gmail.com>
2026-08-08 16:38:17 +0400

    Fix

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -12,8 +12,7 @@ const girisKontrolu = require('../middleware/auth');
 const adminKontrolu = require('../middleware/admin');
 const upload = require('../middleware/upload');
 
-// DIKKAT: /en-cok-satilanlar rotasi /:id'den ONCE tanimlanmali,
-// yoksa "en-cok-satilanlar" bir id olarak algilanir ve hata verir
+
 router.get('/en-cok-satilanlar', enCokSatilanlar);
 
 router.get('/', urunleriListele);
